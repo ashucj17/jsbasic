3b700d91eb02d844dfe9db651ce774c4e30ce6f6
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2024-09-13 16:53:11 +0530

    basics of javascript

---
+++ basic.js
@@ -0,0 +1,50 @@
+//----JavaScript basics------------------
+console.log("basic.js is connected");
+
+// ----------------variables-------------
+
+let firstName = "Raman";
+console.log(firstName);
+firstName = "Asha";
+firstName = 100;
+console.log(firstName);
+
+// const value = 1134;
+// value = 543;
+// console.log(value);
+
+// =============Alert=================
+// alert("Are you sure !!");
+
+//==============Prompt===========
+// prompt("Define Ypurslef");
+
+//==============Data Types============
+
+//-----String-------
+let hobbie = "MLBB";
+console.log(hobbie);
+
+//--------------Number------------
+let num = 100;
+num = 250;
+num = 1200;
+console.log(num);
+
+//------Boolea--------------
+let ans = true;
+let ans1 = ans;
+console.log(ans1);
+
+//____________OBJESTS_____________
+let detailsOfStudent = {
+  name: "Asoop",
+  class: 6,
+  phone: 7418529634,
+  address: "Pune",
+};
+console.log(detailsOfStudent);
+
+//_______________ARRAY_____________
+let Data = [10, 52, 1, 96, 36, 1, 0];
+console.log(Data);
